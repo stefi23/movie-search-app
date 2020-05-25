f60a1cc7856200076f2575dc39331fcba4278f65
stefi23 <stefania.rosca2309@gmail.com>
2020-05-25 12:37:49 +0200

    Updates

---
+++ precache-manifest.eef65d830f74d34e6af9fac0b632df82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92f5773bf510a27dfcc20775c430f592",
+    "url": "/movie-search-app/index.html"
+  },
+  {
+    "revision": "bd087ca19ad7aa67842a",
+    "url": "/movie-search-app/static/css/main.c8242def.chunk.css"
+  },
+  {
+    "revision": "9cb955b3089d5afd280e",
+    "url": "/movie-search-app/static/js/2.24080a62.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-search-app/static/js/2.24080a62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd087ca19ad7aa67842a",
+    "url": "/movie-search-app/static/js/main.519224d6.chunk.js"
+  },
+  {
+    "revision": "4a2439059e9fe967231a",
+    "url": "/movie-search-app/static/js/runtime-main.4829acfe.js"
+  }
+]);
